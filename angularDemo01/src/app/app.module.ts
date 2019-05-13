444eb0fdd05e65a6d2c948bc271ec115940fccea
pablo <pablo@codevelopit.com>
2019-05-12 19:20:08 -0500

    creating homeComponent and table component, adding Navbar in AppComponent and bootstrap carousel

--- angularDemo01/src/app/app.module.ts
+++ angularDemo01/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Router
+import { RouterModule, Route } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TableComponent } from './table/table.component';
+
+const routes: Route[] = [
+  { path: '',     component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'table', component: TableComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
